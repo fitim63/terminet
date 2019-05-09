f3c024c6fa249febaa90011dac96c86e47a0ada8
Fitim63 <fitim8991@gmail.com>
2019-05-09 22:59:34 +0200

    first commit

---
+++ src/main/java/com/terminet/terminet/TerminetApplication.java
@@ -0,0 +1,13 @@
+package com.terminet.terminet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terminet/terminet/TerminetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.terminet.terminet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TerminetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
